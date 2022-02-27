9382e3d4d4d8bf6aa42e05cca0f784ca62c4d702
gengchao <gengchao@tianyancha.com>
2022-02-27 17:04:28 +0800

    p215：第K个最大元素 快速排序解法

--- scripts/p215.py
+++ scripts/p215.py
@@ -18,6 +18,12 @@ from typing import List
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        """
+        插入排序解法
+        :param nums:
+        :param k:
+        :return:
+        """
         lst = [nums[0]]
         for num in nums[1:]:
             if len(lst) == k and lst[-1] < num:
@@ -28,3 +34,49 @@ class Solution:
                     i += 1
                 lst.insert(i, num)
         return lst[-1]
+
+    def partition(self, nums: List[int], l, r) -> int:
+        i, j = l, r
+        value = nums[l]
+        while i < j:
+            while j > i and nums[j] <= value:
+                j -= 1
+            nums[i] = nums[j]
+            while i < j and nums[i] >= value:
+                i += 1
+            nums[j] = nums[i]
+            # nums[i], nums[j] = nums[j], nums[i]
+        nums[i] = value
+        print(nums)
+        return i
+
+    def quickSort(self, nums: List[int], l, r) -> int:
+        if l >= r:
+            return l
+        middle = self.partition(nums, l, r)
+        return middle
+
+    def findKthLargest2(self, nums: List[int], k: int) -> int:
+        """快速排序
+        """
+        l, r = 0, len(nums) - 1
+        middle = self.quickSort(nums, l, r)
+        while middle != k-1:
+            if middle < k-1:
+                middle = self.quickSort(nums, middle+1, r)
+            else:
+                middle = self.quickSort(nums, l, middle - 1)
+        return nums[k-1]
+
+
+
+if __name__ == '__main__':
+    obj = Solution()
+    # lst = [3,2,3,1,2,4,5,5,6]
+    # lst = [28, 2, 3235, 4]
+    # obj.quickSort(lst, 0, len(lst)-1)
+    #
+    # # print(obj.findKthLargest2( 2))
+    # # 5
+    print(obj.findKthLargest2([3,2,3,1,2,4,5,5,6], 4))
+    # 4
